b6411663b722ed3f787da0e91664ac62a20a055d
Clm-Roig <clm.roig@gmail.com>
2022-11-18 13:54:03 +0000

    Deploying to gh-pages from @ Clm-Roig/mizk@615e0231c9d42e09704e671396d61ada624572ae 🚀

--- src/theme/fonts.ts
+++
@@ -1,7 +0,0 @@
-const fonts = {
-  body: 'system-ui, sans-serif',
-  heading: 'Georgia, serif',
-  mono: 'Menlo, monospace',
-};
-
-export default fonts;
--- src/theme/index.ts
+++
@@ -1,8 +0,0 @@
-import { extendTheme } from '@chakra-ui/react';
-
-import colors from './colors';
-import fonts from './fonts';
-
-const theme = extendTheme({ colors, fonts });
-
-export default theme;
