5752b2e9ca13eff02c55aae1a82e61c50f659cb0
Clément Roig <clm.roig@gmail.com>
2022-11-22 12:08:21 +0100

    fix(homePage):  sort tools by type

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,10 +3,15 @@ import ToolList from '../components/ToolList';
 import tools from '../data/tools';
 
 function Home() {
+  const sortedTools = tools.sort((a, b) => {
+    if (a.type.name > b.type.name) return 1;
+    if (a.type.name < b.type.name) return -1;
+    return 0;
+  });
   return (
     <>
       <Heading as="h1">All our tools</Heading>
-      <ToolList tools={tools} />
+      <ToolList tools={sortedTools} />
     </>
   );
 }
