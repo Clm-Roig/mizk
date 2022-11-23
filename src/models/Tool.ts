789fb29608709569123c648be631b88b65f78779
Clément Roig <clm.roig@gmail.com>
2022-11-23 10:42:10 +0100

    enhancement(menu): improve menu ux

--- src/models/Tool.ts
+++ src/models/Tool.ts
@@ -3,6 +3,7 @@ import { ToolType } from './ToolType';
 export interface Tool {
   description: string;
   keywords: Array<string>;
+  menuName: string;
   name: string;
   type: ToolType;
   url: string;
